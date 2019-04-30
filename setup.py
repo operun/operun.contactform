25e38271f5c509c07eb303878abaa6ab84288ae0
Jesse Stippel <jesse.stippel@operun.de>
2019-04-30 09:42:23 +0200

    Preparing release 1.0.0a1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='operun.contactform',
-    version='1.0.0a1.dev0',
+    version='1.0.0a1',
     description="A Plone Contact-Form Add-On.",
     long_description=long_description,
     classifiers=[
