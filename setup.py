f14c5c4cd144c7ccd2b7fca54a8e627785edd510
Jesse Stippel <jesse.stippel@operun.de>
2019-06-18 11:38:33 +0200

    Back to development: 1.0.0a5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='operun.contactform',
-    version='1.0.0a4',
+    version='1.0.0a5.dev0',
     description="A Plone Contact-Form Add-On.",
     long_description=long_description,
     classifiers=[
